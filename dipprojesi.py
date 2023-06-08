b4dd5fd24b48035125226c6d2d6215786c961907
Sefa Akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2023-06-08 14:55:13 +0300

    update

---
+++ dipprojesi.py
@@ -0,0 +1,170 @@
+import tkinter
+from tkinter import filedialog
+from PIL import Image, ImageTk
+from tkinter import ttk
+import filter
+import file
+import dosyaacma
+global a 
+program = tkinter.Tk()
+photo = Image.open("C:/Users/SEFA/Desktop/bg.png")#open image
+resize = photo.resize((1920,1080),Image.ANTIALIAS)
+bgimg = ImageTk.PhotoImage(resize)
+bglabel = tkinter.Label(program,image=bgimg,anchor="center")
+bglabel.pack()
+program.title("Digital Image Processing")
+program.geometry("1920x1080")
+def name_of_filter():
+    name = clicked.get()
+    return name
+clicked = tkinter.StringVar(program)
+clicked.set("Select an option:")
+drop = tkinter.OptionMenu(program,clicked,"Negative","Blur","Contour","Detail","Edge","Find Edge","Emboss","Sharp","Smooth","Minimum","Median","Maximum","Gauss")
+drop.config(bg="#4A4242", fg="WHITE",width=18,height=2,font= ("Arial", 10))
+def filtre_adi():#açılır pencereden filtre adı verecek.
+    return clicked.get()
+def get_name():
+    file = dosyaacma.foto_yukle()
+    return file
+def filtreliyi_goster():#programa fotoğrafı yerleştirecek.
+    filtre_adi = clicked.get()
+    if(filtre_adi == "Gauss"):
+        filtreli_foto = filter.gauss(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Blur"):
+        filtreli_foto = filter.blur(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Contour"):
+        filtreli_foto = filter.contour(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Detail"):
+        filtreli_foto = filter.detail(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Edge"):
+        filtreli_foto = filter.image_edge(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Find Edge"):
+        filtreli_foto = filter.find_edge(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Emboss"):
+        filtreli_foto = filter.emboss(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Sharp"):
+        filtreli_foto = filter.sharp(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Smooth"):
+        filtreli_foto = filter.smooth(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Minimum"):
+        filtreli_foto = filter.min(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Median"):
+        filtreli_foto = filter.median(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Maximum"):
+        filtreli_foto = filter.max(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+    elif(filtre_adi == "Negative"):
+        filtreli_foto = filter.negative(a)
+        photo = Image.open(filtreli_foto)#open image
+        resize = photo.resize((300,400),Image.ANTIALIAS)
+        new_pic = ImageTk.PhotoImage(resize)
+        label2 = tkinter.Label(program,image=new_pic,anchor="center")
+        label2.place(relx = 0.53, rely = 0.8, anchor = "center")
+        program.mainloop()
+
+
+def goster():
+    global a 
+    a = dosyaacma.foto_yukle()
+    photo = Image.open(a)#open image
+    resize = photo.resize((300,400),Image.ANTIALIAS)
+    original = ImageTk.PhotoImage(resize)
+    label1 = tkinter.Label(program,image = original)
+    label1.place(relx = 0.45, rely = 0.1)
+    #filtreliyi_goster(a,filtre_adi())#filtreli halini bastiracak.
+    program.mainloop()
+def goster_foto():
+    global a
+    a = file.foto()
+    photo = Image.open(a)#open image
+    resize = photo.resize((300,400),Image.ANTIALIAS)
+    original = ImageTk.PhotoImage(resize)
+    label1 = tkinter.Label(program,image = original)
+    label1.place(relx = 0.45, rely = 0.1)
+    #filtreliyi_goster(a,filtre_adi())#filtreli halini bastiracak.
+    program.mainloop()
+original = tkinter.Label(program,text = "Original Photo",bg = '#6c3030',fg='white',font= ("Arial", 25))
+filtered = tkinter.Label(program,text = "Filtered Photo",bg = '#6c3030',fg='white',font= ("Arial", 25))
+original.place(relx = 0.475,rely=0.05)
+filtered.place(relx = 0.475,rely=0.55)
+button_dosya = tkinter.Button(program,text = 'Select Folder',command=goster,height=5,width=20,bg = '#4A4242',fg = 'white',font= ("Arial", 10))
+submit_button = tkinter.Button(program, text='Submit',command = filtreliyi_goster,bg = '#4A4242',fg='white',width=20,height=2,font= ("Arial", 10)) 
+button_foto =tkinter.Button(program,text = "Take a Photo",command=goster_foto,bg = '#4A4242',fg='white',width=20,height=2,font= ("Arial", 10))
+button_dosya.place(relx = 0.1,rely = 0.150,anchor="w")
+button_foto.place(relx = 0.1,rely = 0.3,anchor="w")
+drop.place(relx = 0.1,rely = 0.45,anchor="w")
+submit_button.place(relx = 0.1,rely = 0.6,anchor="w",)
+
+
+program.mainloop()
---
+++ filter.py
@@ -0,0 +1,89 @@
+from PIL import Image, ImageFilter, ImageOps, ImageTk
+def resim_al():
+    resim = input()
+    return resim
+def negative(resim):
+    img = Image.open(resim)
+    for i in range(0, img.size[0]-1):
+        for j in range(0, img.size[1]-1):
+            pixelColorVals = img.getpixel((i,j));
+            redPixel    = 255 - pixelColorVals[0]; # Negate red pixel
+            greenPixel  = 255 - pixelColorVals[1]; # Negate green pixel
+            bluePixel   = 255 - pixelColorVals[2];
+            img.putpixel((i,j),(redPixel, greenPixel, bluePixel))
+    imgGray = img.convert('L')
+    imgGray.save('C:/Users/SEFA/Desktop/PYTHON/negative.png')
+    return 'C:/Users/SEFA/Desktop/PYTHON/negative.png'
+
+def blur(resim):
+    image = Image.open(resim)
+    image_blur = image.filter(ImageFilter.BLUR)
+    temp = image_blur.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/blur.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/blur.png'
+def contour(resim):
+    image = Image.open(resim)
+    image_contour = image.filter(ImageFilter.CONTOUR)
+    temp = image_contour.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/contour.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/contour.png'
+def detail(resim):
+    image = Image.open(resim)
+    image_detail = image.filter(ImageFilter.DETAIL)
+    temp = image_detail.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/detail.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/detail.png'
+def image_edge(resim):
+    image = Image.open(resim)
+    image_edge = image.filter(ImageFilter.EDGE_ENHANCE)
+    temp = image_edge.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/edge.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/edge.png'
+def find_edge(resim):
+    image = Image.open(resim)
+    image_find_edges = image.filter(ImageFilter.FIND_EDGES)
+    temp = image_find_edges.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/findedge.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/findedge.png'
+def emboss(resim):
+    image = Image.open(resim)
+    image_emboss = image.filter(ImageFilter.EMBOSS)
+    temp = image_emboss.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/emboss.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/emboss.png'
+def sharp(resim):
+    image = Image.open(resim)
+    image_sharp = image.filter(ImageFilter.SHARPEN)
+    temp = image_sharp.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/sharp.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/sharp.png'
+def smooth(resim):
+    image = Image.open(resim)
+    image_smooth = image.filter(ImageFilter.SMOOTH)
+    temp = image_smooth.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/smooth.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/smooth.png'
+def min(resim):
+    image = Image.open(resim)
+    image_filtered_min = image.filter(ImageFilter.MinFilter(size = 5))
+    temp = image_filtered_min.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/min.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/min.png'
+def median(resim):
+    image = Image.open(resim)
+    image_filtered_median = image.filter(ImageFilter.MedianFilter(size = 5))
+    temp = image_filtered_median.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/median.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/median.png'
+def max(resim):
+    image = Image.open(resim)
+    image_filtered_max = image.filter(ImageFilter.MaxFilter(size = 5))
+    temp = image_filtered_max.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/max.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/max.png'
+def gauss(resim):
+    image = Image.open(resim)
+    image_gaussblur = image.filter(ImageFilter.GaussianBlur(radius = 10))
+    temp = image_gaussblur.resize((300,400),Image.ANTIALIAS)
+    temp.save("C:/Users/SEFA/Desktop/PYTHON/gauss.png")
+    return 'C:/Users/SEFA/Desktop/PYTHON/gauss.png'
