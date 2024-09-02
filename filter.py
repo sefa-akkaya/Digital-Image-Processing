850b97222b50a05a3643ce9b2468bf8fdf397ffa
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-09-02 11:35:30 +0300

    TASK-11 asfsadfasdf

--- filter.py
+++ filter.py
@@ -5,7 +5,7 @@ def resim_al():
 def negative(resim):
     img = Image.open(resim)     
     for i in range(0, img.size[0]-1): 
-        for j in range(0, img.size[1]-1):
+        for j in range(0, img.size[1]-1): 
             pixelColorVals = img.getpixel((i,j));
             redPixel    = 255 - pixelColorVals[0]; # Negate red pixel
             greenPixel  = 255 - pixelColorVals[1]; # Negate green pixel
