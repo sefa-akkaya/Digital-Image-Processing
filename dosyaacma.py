af86cf94dc2d8c22f1c7697bfac052ea2e6b4c6f
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-09-18 15:07:52 +0300

    HVLSN-29

--- dosyaacma.py
+++ dosyaacma.py
@@ -2,7 +2,7 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename 
  
 def foto_yukle():  
-    Tk().withdraw()    
+    Tk().withdraw()     
     filename = askopenfilename() 
     return filename   
    
